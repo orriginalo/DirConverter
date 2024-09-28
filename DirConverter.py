118f626501d65e3ffe99080e8ba80741ffc469df
orriginalo <originalo1717@gmail.com>
2024-09-28 17:37:13 +0400

    multipotok test 1

--- DirConverter.py
+++ DirConverter.py
@@ -5,6 +5,7 @@ from rich.console import Console
 from pydub import AudioSegment
 from time import perf_counter
 from wand.image import Image
+from concurrent.futures import ThreadPoolExecutor
 # pip install rich pydub Wand
 
 console = Console()
@@ -99,23 +100,24 @@ def main():
 	print(f"2. Images to [bold]{pictures_convertto}")
 	variant = list(dict.fromkeys([int(x) for x in input("Enter your choice: ").split() if int(x) <= 2]))
 	
-	if has_dirs(path):
-		if ask("[bold]Go through all the directories in this folder?"):
-			for root, dirs, files in os.walk(path):
-				for dir in dirs:
-					for i in variant:
-						match i:
-							case 1:
-								convert_audio(os.path.join(root, dir))
-							case 2:
-								convert_photo(os.path.join(root, dir))
-	else:
-		for i in variant:
-			match i:
-				case 1:
-					convert_audio(path)
-				case 2:
-					convert_photo(path)
+	with ThreadPoolExecutor() as executor:
+		if has_dirs(path):
+			if ask("[bold]Go through all the directories in this folder?"):
+				for root, dirs, files in os.walk(path):
+					for dir in dirs:
+						for i in variant:
+							match i:
+								case 1:
+									executor.submit(convert_audio, os.path.join(root, dir))
+								case 2:
+									executor.submit(convert_photo, os.path.join(root, dir))
+		else:
+			for i in variant:
+				match i:
+					case 1:
+						executor.submit(convert_audio, os.path.join(root, dir))
+					case 2:
+						executor.submit(convert_photo, os.path.join(root, dir))
 				
 	print(f"[bold green]Files converted successfully! [/bold green][bold]Total elapsed time: [bold blue]{sum(total_elapsed_time):.1f} sec")
 	
